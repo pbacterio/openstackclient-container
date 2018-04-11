67bcb265f8430df60cf36e21a37f2ac6c6922527
German Moya <german.moya@bbva.com>
2018-04-11 16:02:21 +0200

    * Add support for `--os-cacert`, `--os-cert`, `--os-key` flags. * Compress executable with upx.

--- openstackclient-container.go
+++ openstackclient-container.go
@@ -3,61 +3,71 @@ package main
 import (
 	"os"
 	"strings"
-	"io/ioutil"
 	"path/filepath"
 	"fmt"
+	//"os/exec"
+	"math/rand"
+	"time"
 	"os/exec"
 )
 
 var dockerImage = "pbacterio/openstackclient-docker"
 
 var varsPointingFiles = map[string]bool{
-	"OS_CACERT": true,
-	"OS_CERT":   true,
-	"OS_KEY":    true,
+	"OS_CACERT":   true,
+	"--os-cacert": true,
+	"OS_CERT":     true,
+	"--os-cert":   true,
+	"OS_KEY":      true,
+	"--os-key":    true,
 }
 
 func main() {
-	dockerArgs := []string{"run", "--rm", "-it",}
+	files := map[string]string{}
+	dockerArgs := []string{}
 
-	varsToFiles := make([]string, 0, len(varsPointingFiles))
-
-	for _, e := range os.Environ() {
-		envVar := strings.Split(e, "=")[0]
-		if varsPointingFiles[envVar] {
-			varsToFiles = append(varsToFiles, envVar)
+	// Get enviroment variables
+	for _, env := range os.Environ() {
+		envParts := strings.SplitN(env, "=", 2)
+		name, value := envParts[0], envParts[1]
+		if !strings.HasPrefix(name, "OS_") {
 			continue
 		}
-		if strings.HasPrefix(envVar, "OS") {
-			dockerArgs = append(dockerArgs, "-e", e)
+		if !varsPointingFiles[name] {
+			dockerArgs = append(dockerArgs, "-e", name+":"+value)
+			continue
 		}
+		mapped_value := files[value]
+		if mapped_value == "" {
+			mapped_value = "/" + randName() + filepath.Base(value)
+			files[value] = mapped_value
+			dockerArgs = append([]string{"-v", value + ":" + mapped_value}, dockerArgs...)
+		}
+		dockerArgs = append(dockerArgs, "-e", name+":"+mapped_value)
 	}
 
-	if len(varsToFiles) > 0 {
-		tmpVol, err := ioutil.TempDir("", "openstack-docker")
-		if err != nil {
-			panic(err)
+	dockerArgs = append(dockerArgs, "pbacterio/openstackclient-docker", "openstack")
+
+	// Get args-vars
+	for i := 1; i < len(os.Args); i++ {
+		arg := os.Args[i]
+		if !varsPointingFiles[arg] || len(os.Args) < i+2 {
+			dockerArgs = append(dockerArgs, arg)
+			continue
 		}
-		dockerArgs = append(dockerArgs, "-v", tmpVol+":/certs_vol")
-		for _, varName := range varsToFiles {
-			tmpDir, err := ioutil.TempDir(tmpVol, "")
-			if err != nil {
-				panic(err)
-			}
-			srcPath := os.Getenv(varName)
-			err = copyFile(srcPath, filepath.Join(tmpDir, filepath.Base(srcPath)))
-			if err != nil {
-				panic(err)
-			}
-			dockerArgs = append(dockerArgs, "-e",
-				fmt.Sprintf("%v=/certs_vol/%v/%v", varName, filepath.Base(tmpDir), filepath.Base(srcPath)))
+		mapped_value := files[os.Args[i+1]]
+		if mapped_value == "" {
+			mapped_value = "/" + randName() + filepath.Base(os.Args[i+1])
+			files[os.Args[i+1]] = mapped_value
+			dockerArgs = append([]string{"-v", os.Args[i+1] + ":" + mapped_value}, dockerArgs...)
 		}
-		defer os.RemoveAll(tmpVol)
+		dockerArgs = append(dockerArgs, arg, mapped_value)
+		i++
 	}
 
-	dockerArgs = append(dockerArgs, dockerImage, "openstack")
-	dockerArgs = append(dockerArgs, os.Args[1:]...)
-
+	// Run openstackclient container
+	dockerArgs = append([]string{"run", "--rm", "-it"}, dockerArgs...)
+	fmt.Println(dockerArgs)
 	cmd := exec.Command("docker", dockerArgs...)
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
@@ -65,10 +75,10 @@ func main() {
 	cmd.Run()
 }
 
-func copyFile(src string, dst string) error {
-	data, err := ioutil.ReadFile(src)
-	if err != nil {
-		return err
-	}
-	return ioutil.WriteFile(dst, data, 0644)
+func randName() string {
+	return fmt.Sprintf("%X", rand.Uint64())
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
